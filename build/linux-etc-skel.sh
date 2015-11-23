d155931fb75f65c58a1d9dae1fe1410ad53ff34d
Tien Nguyen <al1man@hva.online>
2015-11-23 17:10:37 +0700

    Change some permissions of files.

--- build/linux-etc-skel.sh
+++ build/linux-etc-skel.sh
@@ -6,3 +6,5 @@ wget -O $HOME/.bash_profile https://raw.githubusercontent.com/htien/linux-etc-sk
 wget -O $HOME/.bashrc https://raw.githubusercontent.com/htien/linux-etc-skel/master/bashrc
 wget -O $HOME/.gitconfig https://raw.githubusercontent.com/htien/linux-etc-skel/master/gitconfig
 wget -O $HOME/.ssh/config https://raw.githubusercontent.com/htien/linux-etc-skel/master/.ssh/config
+
+chmod -R u=rwX,go= $HOME/.bash* $HOME/.ssh
